8047f93608acceb19759ab0aa84116caba1887fb
ImbOut <78740229+ImbOut@users.noreply.github.com>
2021-02-10 10:26:15 +0900

    Update SkLMN.py

--- SkLMN.py
+++ SkLMN.py
@@ -16,14 +16,13 @@ from sklearn.ensemble import IsolationForest
 from sklearn import preprocessing
 from tqdm import tqdm
 class SkLMN(ClassifierBase):
-    #VINH VO
+    
     def map_index(self, index, samples):
         for i in range (0, len(samples)):
             if samples[i] == index:
                 return i
-        #print("samples = ", samples)
-        #print(str(index) + " is at " + str(i))
         return -1
+    
     def calculate_cardinality_of_node(self, x, y, node_indicator):
         x_row = node_indicator[x]
         y_row = node_indicator[y]
@@ -31,6 +30,7 @@ class SkLMN(ClassifierBase):
             if x_row[i] != y_row[i]:
                 return np.sum(node_indicator[:,i-1])
         return 0
+    
     def extract_samples(self, X, indices):
         dataPoints = []
         for i in range (0, len(indices)):
@@ -52,6 +52,7 @@ class SkLMN(ClassifierBase):
         if len(tree_list) ==0:
             return float('inf')
         return cardinality/len(tree_list)
+    
     def calculate_m_e_a_tree(self,x, y,i, estimators_sample):
         cardinality = 0
         if self.all_indexces[i][x]==-1:
@@ -64,18 +65,15 @@ class SkLMN(ClassifierBase):
         node_indicator = self.all_indicators[i]
         cardinality = self.calculate_cardinality_of_node( x2, y2, node_indicator)
         return cardinality
+    
     def cal_m_e_matrix(self,X, estimators, estimators_samples):
         temp_dict2 = {}
         for i in range(len(estimators)):
-            #print("\nProcessing trees",i,":",end=' ')
             estimators_sample = estimators_samples[i]
             step = max( int(len(estimators_sample)/100)-1,1)
             estimator = estimators[i]
             
             for j1 in tqdm(range(len(estimators_sample)-1),"Tree " +str(i)):
-                #if (j1 % step == 0):
-                    #print(int(j1 / step),end=' ') 
-
                 x = estimators_sample[j1]
                 for j2 in range(j1+1,len(estimators_sample)):
                     y = estimators_sample[j2]
@@ -86,8 +84,9 @@ class SkLMN(ClassifierBase):
                     if y in temp_dict2:
                         temp_dict2[y].append((x,val))
                     else: temp_dict2[y] = [(x,val)]
+                        
         print("Finish Processing all trees, Calculating distance Matrix")
-        #od = collections.OrderedDict(sorted(temp_dict2.items()))
+        
         m_list = [[]  for i in range(len(X))]
         for i,value in temp_dict2.items():
             if i >= self.ax0_max :m_list[i]=[]; continue;
@@ -95,7 +94,6 @@ class SkLMN(ClassifierBase):
             temp_dict_count = [0 for i in range(len(X))] 
             for j,val in value:
                 if j < self.ax1_min: continue;
-                #print(i,j,val)
                 temp_dict[j]+= val
                 temp_dict_count[j] += 1
             for j in range(len(temp_dict)):
@@ -105,10 +103,7 @@ class SkLMN(ClassifierBase):
                 else : 
                     temp_dict[j]/=temp_dict_count[j]
                 temp_dict[i]=0
-                #m_list.append(temp_dict)
                 m_list[i]=temp_dict
-            #if (i % 500 == 0):
-                #print("Finished Processing Data point ", i)
         for i in range(len(m_list)):
             if(m_list[i]==[]):
                 m_list[i] = [1000000000  for i in range(len(X))]
@@ -157,7 +152,6 @@ class SkLMN(ClassifierBase):
                 m_list[i] = [1000000000  for i in range(len(X))]
         return m_list
 
-
     def cal_m_e_matrix_VINH_VO(self,X, estimators, estimators_samples):
         m_list = []
         for x in range (0, X.shape[0]):
@@ -165,11 +159,8 @@ class SkLMN(ClassifierBase):
             trees_tmp_toan = [i for i in range(len(estimators_samples)) if x in estimators_samples[i] ]
             for y in range (0, X.shape[0]):
                 if y != x:
-                    #trees = [i for i in range(len(estimators_samples)) if (x in estimators_samples[i] and y in estimators_samples[i])]
                     trees = [i for i in trees_tmp_toan if (y in estimators_samples[i])]
                     m_e = self.calculate_m_e(x, y, X, trees, estimators, estimators_samples)
-                    #key_str = "(" + str(x) + "," + str(y) + ")"
-                    #temp_dict[key_str] = m_e
                     temp_dict.append(m_e)
                 else: temp_dict.append(0)
             m_list.append(temp_dict)
@@ -178,6 +169,7 @@ class SkLMN(ClassifierBase):
                 print("Finished Processing Data point ", x)
         #print(m_list)
         return m_list
+    
     def ComputeAllIndicators(self,clf):
         print("Now! Start MLOS 3")
         self.all_indexces = [[-1 for j in range(len(self.data))] for i in range(len(clf.estimators_))]
@@ -201,11 +193,6 @@ class SkLMN(ClassifierBase):
     def Fit(self):
         self.name = 'SkLMN'
         time_start = timeit.default_timer()
-        #self.std_scale = preprocessing.StandardScaler().fit(self.X_train)
-        #self.X_train = self.std_scale.transform(self.X_train)
-        #self.X_test = self.std_scale.transform(self.X_test)
-        #self.neigh = KNeighborsClassifier(n_neighbors=TDef.k)
-        #self.neigh.fit(self.X_train, self.y_train) 
         GMM =[]
         for i in range(self.n_class):
             l = self.class_labels[i]
@@ -222,19 +209,12 @@ class SkLMN(ClassifierBase):
             GM.lower_bound_ += GMM[i].lower_bound_
             asd=123
         GM.lower_bound_/= self.n_class
-        #GM2 = GaussianMixture(n_components=2, random_state=0).fit(self.X_train)
-        #a1= GM.predict_proba(self.X_train)
-        #a2= GM2.predict_proba(self.X_train)
-        #asd=123
         confidents =  GM.predict_proba(self.X_train)
         self.confidents =np.zeros(self.N_train)
         for i in range(self.N_train):
             self.confidents[i] = confidents[i][int(self.y_train[i])]
 
         asd=123
-        
-
-
         self.fit_time = timeit.default_timer()-time_start
     def MLOSDistance(self):
         self.ax0_min = 0
@@ -253,36 +233,23 @@ class SkLMN(ClassifierBase):
             m_e_matrix = self.cal_m_e_matrix_single(self.data, clf.estimators_, clf.estimators_samples_)
         else: 
             m_e_matrix = self.cal_m_e_matrix(self.data, clf.estimators_, clf.estimators_samples_)
-        
-        #m_e_matrix = self.cal_m_e_matrix_VINH_VO(self.data, clf.estimators_, clf.estimators_samples_)
+ 
         dist_matrix = np.array(m_e_matrix)[0:self.N_train ,self.N_train:self.N]
         return dist_matrix
-        #for i in range(self.N):
-        #    dists = m_e_matrix[i]
-        #    indexes_ = np.argsort(dists)[1:TDef.k+1]
-        #    distances_ = np.array(dists) [indexes_]
-        #    for j in range(TDef.k):
-        #        distances[i][j] = distances_[j]
-        #        indexes[i][j] = indexes_[j]
-
-        #dist_matrixmlos = 
-        
+       
     def Evaluate(self):
         time_start = timeit.default_timer()
 
-        
         #############################################################
         dist_matrix = self.MLOSDistance()
         #############################################################3
-        #dist_matrix  = scipy.spatial.distance.cdist( self.X_train,self.X_test)
+        
         dist_matrix/= np.max(dist_matrix)
-        #D = np.sqrt(np.sum((self.X_test[:, None, :] - self.X_train[None, :, :])**2, axis = -1))
         k_i = np.argpartition(dist_matrix, TDef.k, axis=0)[:TDef.k]
         k_d = np.take_along_axis(dist_matrix, k_i, axis = 0)
         k_i=k_i.transpose()
         k_d=k_d.transpose()
-        #self.y_pred = self.neigh.predict(self.X_test)
-        #self.y_prob = self.neigh.predict_proba(self.X_test)
+        
         self.y_prob = np.zeros((self.N_test,self.n_class))
         for i in range(self.N_test):
             for j in range(TDef.k):
